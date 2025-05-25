9b2d7461d22a40f57196d5480d44985329920095
Muhammad Awais <137446210+muhammad7865@users.noreply.github.com>
2025-05-25 13:07:55 +0500

    DashBoard-Issue

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,17 +1,45 @@
+import { EllipsisVertical } from 'lucide-react'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
 import CartButton from './cart-button'
 import UserButton from './user-button'
 
 
-
-export default function Menu({ forAdmin = false }: { forAdmin?: boolean }) {
+const Menu = ({ forAdmin = false }: { forAdmin?: boolean }) => {
+ 
   return (
     <div className='flex justify-end'>
-      <nav className=' hidden md:flex gap-3 w-full'>
+      <nav className='md:flex gap-3 hidden w-full'>
       
-      <UserButton/>
-      {forAdmin ? null : <CartButton />}
+        <UserButton />
+        {forAdmin ? null : <CartButton />}
       </nav>
-  
+      <nav className='md:hidden'>
+        <Sheet>
+          <SheetTrigger className='align-middle header-button'>
+            <EllipsisVertical className='h-6 w-6' />
+          </SheetTrigger>
+          <SheetContent className='bg-black text-white  flex flex-col items-start  '>
+            <SheetHeader className='w-full'>
+              <div className='flex items-center justify-between '>
+                <SheetTitle className='  '>{('Header.Site Menu')}</SheetTitle>
+                <SheetDescription></SheetDescription>
+              </div>
+            </SheetHeader>
+
+            <UserButton />
+            <CartButton />
+          </SheetContent>
+        </Sheet>
+      </nav>
     </div>
   )
 }
+
+export default Menu
